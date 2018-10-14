d600ec2cef776b084a4933019896f73c426874f6
Dmitry Bohdanov <rainsxng@gmail.com / starsettime@gmail.com>
2018-10-14 14:52:14 +0300

    Project has been created

---
+++ app/controllers/AppController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\AppModel;
+use ishop\base\Controller;
+
+class AppController extends Controller{
+
+    public function __construct($route){
+        parent::__construct($route);
+        new AppModel();
+    }
+    public function indexAction(){
+
+    }
+
+}
---
+++ app/controllers/MainController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\controllers;
+
+use ishop\Cache;
+
+class MainController extends AppController {
+
+    public function indexAction(){
+        $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
+    }
+
+}
