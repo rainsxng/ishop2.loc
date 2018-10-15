12379f5b429ea439764eda7499ded2da55e6f9ac
Dmitry Bohdanov <rainsxng@gmail.com / starsettime@gmail.com>
2018-10-16 02:28:47 +0300

    Realized brands showcase

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -7,6 +7,8 @@ use ishop\Cache;
 class MainController extends AppController {
 
     public function indexAction(){
+        $brands = \R::find('brand','LIMIT 3');
+        $this->set(compact('brands'));
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
     }
 
